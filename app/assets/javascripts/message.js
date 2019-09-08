107ea741b87ca1410adce65bfcd847a6420b9a02
shiotamotoki <shiota1974@gmail.com>
2019-09-08 11:11:36 +0900

    create-auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,29 @@
 $(function(){
   function buildMessage(message){
-    var content = message.content ? `${ message.content}` : "";
-    var img = message.image ? `<img sec= ${ message.image }>` : "";
-    var html = `<div class="messages-message">
+    var image = message.image.url ? `<img src= ${ message.image.url }>` : "";
+
+    var html = `<div class="messages-message" data-message-id="${message.id}">
                   <div class="messages-message__upper-info">
                     <div class="messages-message__upper-info__talker">
                       ${message.user_name}
                     </div>
                     <div class="messages-message__upper-info__date">
-                      ${message.date}
+                      ${message.created_at}
                     </div>
                   </div>
                   <div class="messages-message__text">
                     <p class="messages-message__text__content">
                       <div>
-                      ${content}
+                      ${message.content}
                       </div>
-                      ${img}
+                      ${image}
                     </p>
                   </div>
                  </div>`
     return html;
   }
 
-  $('#new_message'). submit(function(e){
+  $('#new_message').submit(function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -46,4 +46,28 @@ $(function(){
     })
     return false;
   })
-})
+
+  var reloadMessages = function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.messages-message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML = buildMessage(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function(){
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
